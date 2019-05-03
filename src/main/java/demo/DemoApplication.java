565c6cbf3fc0fede733d6b0f4ee4cac0c0e65b04
Miłosz <milosz.gus@gmail.com>
2019-05-03 14:36:26 +0200

    Initial commit

---
+++ src/main/java/demo/DemoApplication.java
@@ -0,0 +1,19 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        ApplicationContext ctx = SpringApplication.run(DemoApplication.class, args);
+        InjectedByConstructorService constructorService = (InjectedByConstructorService) ctx.getBean("injectedByConstructorService");
+
+        constructorService.getMessage();
+
+        SetterBasedService setterBasedService=(SetterBasedService) ctx.getBean("setterBasedService");
+        setterBasedService.getMessage();
+    }
+}
